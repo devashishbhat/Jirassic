1bca1c1de03016293b6889e78f971d92f064ebad
saniddhyaDubey <dubeysaniddhya@gmail.com>
2025-02-02 00:55:43 -0500

    task generator through Google 1.0 Pro Code

--- backend/server/routes/user_routes.py
+++ backend/server/routes/user_routes.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Depends
 from models.user import UserRegister, UserLogin
+from models.transcript import transcriptRequestBody
+# from fastapi.middleware.cors import CORSMiddleware
 from services.user import register_user_service, login_user_service
 from services.database import get_database
+from services.task_generator import assign_tasks_from_transcript
 
 user_router = APIRouter()
 
@@ -12,3 +15,7 @@ async def register_user(user_data: UserRegister, db=Depends(get_database)):
 @user_router.post("/login")
 async def login_user(user_data:UserLogin, db=Depends(get_database)):
     return await login_user_service(user_data, db)
+
+@user_router.post("/generate-task")
+async def task_generator(generatedTranscript: transcriptRequestBody):
+    return assign_tasks_from_transcript(generatedTranscript)
